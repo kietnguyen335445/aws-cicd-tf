52aab55b0a04a0091a1a53b73e32b724fa9b7ca5
tkiet39 <ntkiet3093@gmail.com>
2025-03-31 14:04:26 +0000

    Edit output.tf

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -15,3 +15,15 @@ output "public_subnet_ids" {
     aws_subnet.public_subnet_2.id
   ]
 }
+
+output "private_route_table_id" {
+  value = aws_route_table.private_route_table.id
+}
+
+output "public_route_table_id" {
+  value = aws_route_table.public_route_table.id
+}
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.nat_gateway.id
+}
